cc15c6305f7e11e76d03b77e00828a948c46b021
manhaevedetlef <58568927+manhaevedetlef@users.noreply.github.com>
2023-03-24 16:11:59 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 //variabelen
 let steps=0
-input.onButtonPressed(Button.A,(){
+//functie1
+input.onButtonPressed(Button.A,()=>{
     steps=0
+    basic.showNumber(steps)
+})//einde functie 1
+
+input.onButtonPressed(Button.B,()=>{
     basic.showNumber(steps)
 })
 
-
-
-
+input.onGesture(Gesture.Shake,()=>{
+    steps= steps+1
+    basic.showNumber(steps)
+})
 
 basic.forever(function () {
 	
